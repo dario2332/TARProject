abcdb243c1b4684d945e5e7f655958557f4b4fe4
Dario Smolcic <dario.smolcic@fer.hr>
2016-06-03 19:52:44 +0200

    Added MRR and P@10

--- Evaluation.py
+++ Evaluation.py
@@ -7,8 +7,12 @@ def evaluate(results_file, query_num, relevances):
         relevant = 0
         precision = 0
         r_precision = None
+        rr = None
+        p_10 = 0
         for i, line in enumerate(lines):
             if line.strip().split()[0] in relevances[query_num]:
+                if relevant == 0: rr = 1.0/ (i+1)
+                if i < 10: p_10 += 1
                 relevant += 1
                 precision += float(relevant) / (i+1)
             if i+1 == len(relevances[query_num]):
@@ -16,10 +20,13 @@ def evaluate(results_file, query_num, relevances):
 
             
         precision = precision / relevant
+        p_10 /= 10.0
         print query_num
         print "AP: ", precision
-        print "R-precision", r_precision
-        return precision, r_precision
+        print "R-precision: ", r_precision
+        print "RR: ", rr
+        print "P@10: ", p_10
+        return precision, r_precision, rr, p_10
 
 def getRelevances():
     relevances = {}
@@ -38,17 +45,24 @@ def getRelevances():
 
 def main():
     relevances = getRelevances()
-    mean_precision, mean_r_precision = 0, 0 
+    mean_precision, mean_r_precision, mrr, mp_10 = 0, 0, 0, 0
 
     for query_num in range(1, 51):
-        precision, r_precision = evaluate("Results/query"+str(query_num)+".txt", query_num, relevances)
+        if (query_num == 9 or query_num == 49): continue
+        precision, r_precision, rr, p_10 = evaluate("Results/query"+str(query_num)+".txt", query_num, relevances)
         mean_precision += precision
         mean_r_precision += r_precision
+        mrr += rr
+        mp_10 += p_10
 
     mean_precision /= 48
     mean_r_precision /= 48
+    mrr /= 48
+    mp_10 /= 48
     print "MAP: ", mean_precision 
-    print "Mean R-precision", mean_r_precision
+    print "Mean R-precision: ", mean_r_precision
+    print "MRR: ", mrr
+    print "MP@10: ", mp_10
     
 
 if __name__ == "__main__":
