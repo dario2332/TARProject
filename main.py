81c8081c8f4cd98f8a302c713e45e0b494955f9d
Dario Smolcic <dario.smolcic@fer.hr>
2016-06-03 14:46:49 +0200

    Combining VSM scores with PageRank scores

--- main.py
+++ main.py
@@ -4,33 +4,32 @@ import numpy as np
 import sys
 import pickle
 from Evaluation import evaluate, getRelevances
+from PageRank import pageRank
+from links import createAdjacencyMatrix, createLinksSmart, getQueryNum
+import math
 
-def getArticleNum(name):
-    return int(name.split('-')[1].split('.')[0])
+def combineScores(vsm_score, pr_score, articles, w=0.7):
+    results = sorted(zip(articles, vsm_score), key = lambda scored_article: scored_article[1])
+    results.reverse()
+    results = map(lambda x: x[0], results)
 
-def getQueryNum(name):
-    return int(name.split('.')[0][5:])
+    rank = np.zeros((len(vsm_score)))
+    for i, article in enumerate(articles):
+        rank[i] = results.index(article) + 1
 
-def createAdjacencyMatrix(articles):
-    H = np.zeros((len(articles), len(articles)))
-    for i, article1 in enumerate(articles):
-        article_num1 = getArticleNum(article1)
-        for j, article2 in enumerate(articles[i+1:]):
-            article_num2 = getArticleNum(article2)
-            if (abs(article_num2-article_num1) <= 200):
-                H[i][j] = 1
-                H[j][i] = 1
-            
-    row_sums = H.sum(axis=1)
-    for i, s in enumerate(row_sums):
-        if s > 0:
-            H[i] /= s
-    return H
-
+    return w * vsm_score + (1-w) * pr_score / (rank + math.log(5))
+    
 
 def main():
     #texts, queries = get_texts()
 
+    #H = createAdjacencyMatrix(texts)
+    #pickle.dump(H, open("Pickle/H200", "wb"))
+    #H = pickle.load(open("Pickle/H200", "rb"))
+  
+    #pr_score = pageRank(H)
+    #pickle.dump(pr_score, open("Pickle/PRScore", "wb"))
+    #pr_score = pickle.load(open("Pickle/PRScore", "rb"))
     texts = pickle.load(open("Pickle/texts", "rb")) 
     queries = pickle.load(open("Pickle/queries", "rb")) 
 
@@ -44,15 +43,26 @@ def main():
     #vsm = pickle.load(open("Pickle/VSM", "rb")) 
     vsm = pickle.load(open("Pickle/BinaryVSM", "rb")) 
 
+    #H = createLinksSmart(map(lambda x: x.text, vsm.articles))
+
+    #pickle.dump(pr_score, open("Pickle/PRScoreSmart", "wb"))
+    pr_score = pickle.load(open("Pickle/PRScoreSmart", "rb"))
     #vsm = VSM(texts)
     #pickle.dump(vsm, open("Pickle/VSM", "wb"))
-    print "loaded"
+    #print "loaded"
     #print vsm.vocabulary
-    print len(vsm.vocabulary)
+    #print len(vsm.vocabulary)
 
     relevances = getRelevances()
     for query in queries:
-        results = vsm.retrieveArticles(query)
+        scores = vsm.retrieveArticles(query)
+        #scores = np.array(scores)
+        #scores = scores * pr_score
+
+        scores = combineScores(np.array(scores), pr_score, map(lambda x: x.name, texts))
+        results = sorted(zip(texts, scores), key = lambda scored_article: scored_article[1])
+        results.reverse()
+
         lines = []
         for res in results:
             lines.append(res[0].name + " " + str(res[1]) + "\n")
